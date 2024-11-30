2c21622553b6ba6c7e66d76e8be3f604fd2a9ef5
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-11-30 22:24:28 +0530

    basic-learning/swiper-core: navigation

---
+++ basic-learning/swiper-core/02-navigation/script.js
@@ -0,0 +1,20 @@
+// var swiper = new Swiper(".mySwiper", {
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// var swiper = new Swiper(".mySwiper", {
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
