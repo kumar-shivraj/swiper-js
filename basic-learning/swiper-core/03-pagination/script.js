2dc32ce401b0eef803f5aac93df6958049242207
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-01 19:22:21 +0530

    basic-learning/swiper-core: pagination

---
+++ basic-learning/swiper-core/03-pagination/script.js
@@ -0,0 +1,11 @@
+// const swiper = new Swiper(".swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
+new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
