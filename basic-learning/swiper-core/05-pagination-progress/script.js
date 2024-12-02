6a533a4aa19ad8925f5ef36bd7a55b6a2aa44ba7
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-02 23:05:31 +0530

    basic-learning/swiper-core: pagination progress

---
+++ basic-learning/swiper-core/05-pagination-progress/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
