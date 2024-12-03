ffa8846480f9015ff80d01597373b0ec983b6d5c
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-03 23:06:12 +0530

    basic-learning/swiper-core: fraction pagination

---
+++ basic-learning/swiper-core/06-fraction-pagination/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
