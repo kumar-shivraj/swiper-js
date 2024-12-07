6454ee253bd98f12fcb5902f6aaf4f5707c915f6
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-07 11:22:06 +0530

    basic-learning/swiper-react: default swiper

---
+++ basic-learning/swiper-react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Default from "./components/Default";
+
+function App() {
+  return (
+    <>
+      <Default />
+    </>
+  );
+}
+
+export default App;
---
+++ basic-learning/swiper-react/src/components/Default/index.jsx
@@ -0,0 +1,23 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// import "swiper/css/bundle";
+import "./styles.css";
+const Default = () => {
+  return (
+    <>
+      <Swiper className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Default;
